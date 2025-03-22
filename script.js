ffcf96aea7566ed5b9d4ea2085db5bbc61a21dc8
01mehran <132054519+01mehran@users.noreply.github.com>
2025-03-22 14:50:46 +0330

    initial cimmit

---
+++ script.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => calc(e.target.dataset.value));
+});
+const calc = (buttonValue) => {
+  if (buttonValue === "=" && output !== "") {
+    output = eval(output.replace("%", "/100"));
+  } else if (buttonValue === "AC") {
+    output = "";
+  } else if (buttonValue === "DEL") {
+    output = output.toString().slice(0, -1);
+  } else {
+    if (output === "" && specialChars.includes(buttonValue)) return;
+    output += buttonValue;
+  }
+  display.value = output;
+};
